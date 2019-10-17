7b78baddcf1825a81850d59052a1e272ed845b17
Owen <owen@cloudmacaca.com>
2019-10-17 16:44:14 +0800

    增加一些Utility

---
+++ Utility/DirectoryVisitor.cs
@@ -0,0 +1,73 @@
+﻿#if (UNITY_EDITOR)
+
+using UnityEngine;
+using UnityEditor;
+
+public class DirectoryVisitor
+{
+    string path;
+
+    const char splitChar = '/';
+
+    public DirectoryVisitor(string path)
+    {
+        path = path.Replace('\\', splitChar);
+
+        if (IsFolder(path) == false)
+        {
+            path = path.Substring(0, path.LastIndexOf(splitChar) + 1);
+        }
+
+        this.path = path.Replace('\\', splitChar);
+    }
+
+    public DirectoryVisitor Enter(string folderName)
+    {
+        if (IsFolder(path) == false)
+            throw new System.NotImplementedException("Path is not a folder.");
+
+        path += folderName + splitChar;
+        return this;
+    }
+    
+    public DirectoryVisitor Back()
+    {
+        if (path[path.Length - 1] != splitChar)
+            throw new System.NotImplementedException("Path is not a folder.");
+
+        path = path.Substring(0, path.LastIndexOf(splitChar, 0, 2));
+        return this;
+    }
+
+    public bool HasFolder(string folderName)
+    {
+        string checkPath = new DirectoryVisitor(path).Enter(folderName).ToString();
+        return AssetDatabase.IsValidFolder(checkPath);
+    }
+
+    public DirectoryVisitor Create(string folderName)
+    {
+        AssetDatabase.CreateFolder(GetPathWithoutSplitChar(), folderName);
+        Debug.Log($"Create Folder:{GetPathWithoutSplitChar()} , {folderName}");
+
+        return this;
+    }
+
+    public string GetPathWithoutSplitChar()
+    {
+        return path.Remove(path.Length - 1);
+    }
+
+    public override string ToString()
+    {
+        return path;
+    }
+
+    static bool IsFolder(string path)
+    {
+        return path[path.Length - 1] == splitChar;
+    }
+
+
+}
+#endif
---
+++ Utility/ScriptObjectUtility.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if (UNITY_EDITOR)
+using UnityEditor;
+using System.Linq;
+#endif
+
+public static class ScriptObjectUtility
+{
+
+    public static T CreateAndAddData<T>(QueryableData parentData, string dataPropertyName, string assetName = "", string assetPath = "") where T : ScriptableObject, IQueryableData
+    {
+        if (assetPath == "")
+        {
+            string assetPathAndName = AssetDatabase.GetAssetPath(parentData);
+            assetPath = assetPathAndName.Substring(0, assetPathAndName.LastIndexOf('/'));
+        }
+
+        assetName = (assetName == "") ? $"Data{parentData.GetData().Count().ToString("D2")}" : assetName;
+
+        T data = CreateScriptObject<T>(assetPath, assetName);
+        
+        AddData(parentData, dataPropertyName, data);
+
+        return data;
+    }
+
+
+    public static T CreateScriptObject<T>(string path, string name) where T : ScriptableObject
+    {
+        T scriptableObject = ScriptableObject.CreateInstance<T>();
+        scriptableObject.name = name;
+
+        string assetNamePath = $"{path}/{name}.asset";
+        AssetDatabase.CreateAsset(scriptableObject, assetNamePath);
+
+        return AssetDatabase.LoadAssetAtPath<T>(assetNamePath);
+    }
+
+
+    public static void AddData(ScriptableObject parentData, string dataPropertyName, ScriptableObject data)
+    {
+        SerializedObject serializedObject = new SerializedObject(parentData);
+        SerializedProperty serializedProperty = serializedObject.FindProperty(dataPropertyName);
+
+        int index = serializedProperty == null ? 0 : serializedProperty.arraySize;
+        serializedProperty.InsertArrayElementAtIndex(index);
+        serializedProperty.GetArrayElementAtIndex(index).objectReferenceValue = data;
+
+        serializedObject.ApplyModifiedProperties();
+    }
+
+
+
+}
